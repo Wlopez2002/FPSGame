4c7bdb8fc7854513ca035824dd3bf6a1add6a366
Wlopez2002 <148375330+Wlopez2002@users.noreply.github.com>
2026-03-22 12:07:14 -0600

    added force push area

---
+++ Objects/WorldRegions/ForcePushArea.gd
@@ -0,0 +1,10 @@
+extends Area3D
+
+@export var acceleration: Vector3;
+
+func _physics_process(delta: float) -> void:
+	for body in get_overlapping_bodies():
+		if body is PlayerBody:
+			body._applyForce(acceleration)
+		elif body is CharacterBody3D:
+			body.velocity += acceleration;
