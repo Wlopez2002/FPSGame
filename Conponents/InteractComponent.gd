5449d02b3066e2b5e74ed04a025ff882d41b8210
Wlopez2002 <148375330+Wlopez2002@users.noreply.github.com>
2026-03-30 11:41:22 -0600

    added InteractComponent

--- Conponents/InteractComponent.gd
+++ Conponents/InteractComponent.gd
@@ -1,10 +1,16 @@
-extends Area3D
+extends HitBox
 
 class_name InteractComponent
 
 signal interacted();
 
 @export var interactionName: String;
+@export var hitable = true;
 
 func interact():
+	print("interacted")
 	interacted.emit();
+
+func _hit(damage: int):
+	if hitable:
+		interact();
