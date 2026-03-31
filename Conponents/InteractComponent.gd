f11bd39d64ea60a0e1be9295e0a63856f1002b5e
Wlopez2002 <148375330+Wlopez2002@users.noreply.github.com>
2026-03-31 14:50:02 -0600

    worked on InteractComponent

--- Conponents/InteractComponent.gd
+++ Conponents/InteractComponent.gd
@@ -6,10 +6,17 @@ signal interacted();
 
 @export var interactionName: String;
 @export var hitable = true;
+@export var oneShot = false;
+var wasInteracted = false;
 
 func interact():
 	print("interacted")
-	interacted.emit();
+	if oneShot:
+		if !wasInteracted:
+			interacted.emit();
+			wasInteracted = true;
+	else:
+		interacted.emit();
 
 func _hit(damage: int):
 	if hitable:
