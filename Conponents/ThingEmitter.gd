82db2f45356cdf5c32fac82bc50a2f5558e2df42
Wlopez2002 <148375330+Wlopez2002@users.noreply.github.com>
2026-04-06 12:43:34 -0600

    Worked on emitter

--- Conponents/ThingEmitter.gd
+++ Conponents/ThingEmitter.gd
@@ -1,10 +1,19 @@
 extends Node3D
 
+@onready var timer = $Timer;
+
 @export var scene: PackedScene;
 @export var spawnLocation: Vector3; ## Global
 @export var parent: Node;
+@export var time: float = 0.0;
+@export var emitterMax = 10;
+var emitterCount = 0;
 
 func emit():
+	if emitterCount >= emitterMax:
+		return;
+	
+	emitterCount = emitterCount + 1;
 	var newScene = scene.instantiate();
 	if parent != null:
 		parent.add_child(newScene)
@@ -12,3 +21,10 @@ func emit():
 		get_parent().add_child(newScene)
 	if newScene is Node3D:
 		newScene.global_position = spawnLocation
+	if time != 0.0:
+		timer.start(time)
+	print(emitterCount)
+
+
+func _on_timer_timeout() -> void:
+	emit();
