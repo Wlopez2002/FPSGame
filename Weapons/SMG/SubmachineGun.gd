10a12e4458c96f13383b250c66f147121e9e4d32
Wlopez2002 <148375330+Wlopez2002@users.noreply.github.com>
2026-04-09 10:49:10 -0600

    updated SubmachineGun

--- Weapons/SMG/SubmachineGun.gd
+++ Weapons/SMG/SubmachineGun.gd
@@ -2,13 +2,17 @@ extends WeaponBase
 
 @onready var mesh = $MeshInstance3D;
 @onready var hitRay = $MeshInstance3D/hitray
+@onready var hitRay2 = $MeshInstance3D/hitray2
+@onready var hitRay3 = $MeshInstance3D/hitray3
 @onready var attackSpeedTimer = $AttackSpeedTimer;
 
 @onready var pistolClickPlayer = $WeaponSounds/PistolClickPlayer;
 @onready var pistolFirePlayer = $WeaponSounds/PistolFirePlayer;
 @onready var hitScanEffect = $MeshInstance3D/HitScanBulletShot
+@onready var hitScanEffect2 = $MeshInstance3D/HitScanBulletShot2
+@onready var hitScanEffect3 = $MeshInstance3D/HitScanBulletShot3
 
-const SPREAD = 0.1;
+const SPREAD = 0.15;
 var attacking = false;
 var bodyHolding;
 var RNG = RandomNumberGenerator.new()
@@ -30,16 +34,26 @@ func _attackReleased(_body: CharacterBody3D):
 func _attack(_body: CharacterBody3D):
 	if canAttack and curAmmo > 0:
 		applySpread()
-		var hit = hitRay.get_collider();
-		if hit is HitBox:
-			hit._hit(damage);
+		
+		if hitRay.get_collider() is HitBox:
+			hitRay.get_collider()._hit(damage);
+		elif hitRay2.get_collider() is HitBox:
+			hitRay2.get_collider()._hit(damage);
+		elif hitRay3.get_collider() is HitBox:
+			hitRay3.get_collider()._hit(damage);
+		
 		attackSpeedTimer.start(attackSpeed)
 		canAttack = false;
 		curAmmo -= 1;
 		mesh.mesh.material.albedo_color = Color.RED
 		pistolFirePlayer.play()
+		
 		hitScanEffect.emitting = true;
 		hitScanEffect.restart()
+		hitScanEffect2.emitting = true;
+		hitScanEffect2.restart()
+		hitScanEffect3.emitting = true;
+		hitScanEffect3.restart()
 	elif curAmmo <= 0:
 		pistolClickPlayer.play()
 		attackSpeedTimer.start(attackSpeed)
@@ -49,6 +63,16 @@ func applySpread():
 	hitRay.rotation.y = RNG.randf_range(-SPREAD, SPREAD)
 	hitScanEffect.rotation.x = hitRay.rotation.x
 	hitScanEffect.rotation.y = hitRay.rotation.y
+	
+	hitRay2.rotation.x = RNG.randf_range(-SPREAD, SPREAD)
+	hitRay2.rotation.y = RNG.randf_range(-SPREAD, SPREAD)
+	hitScanEffect2.rotation.x = hitRay2.rotation.x
+	hitScanEffect2.rotation.y = hitRay2.rotation.y
+	
+	hitRay3.rotation.x = RNG.randf_range(-SPREAD, SPREAD)
+	hitRay3.rotation.y = RNG.randf_range(-SPREAD, SPREAD)
+	hitScanEffect3.rotation.x = hitRay3.rotation.x
+	hitScanEffect3.rotation.y = hitRay3.rotation.y
 
 func _on_attack_speed_timer_timeout() -> void:
 	canAttack = true;
